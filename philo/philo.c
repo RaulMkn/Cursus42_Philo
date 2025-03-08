9dc10495797b232d9a00e8a877b3e4f0b99d7587
rmakende <rmakendenguerodriguez@gmail.com>
2025-03-08 17:35:51 +0100

    update: añadidos malloc en el main y modificado el makefile

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: rmakende <rmakende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 19:53:00 by rmakende          #+#    #+#             */
-/*   Updated: 2025/03/06 21:01:21 by rmakende         ###   ########.fr       */
+/*   Updated: 2025/03/08 17:29:57 by rmakende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,31 @@ int	main(int argc, char *argv[])
 	t_philo	*philos;
 	int		i;
 
-	i = 0;
 	if (argc < 5 || argc > 6)
+		return (printf("Error: Invalid number of arguments\n"), 1);
+	data.num_philos = ft_atoi(argv[1]);
+	data.time_to_die = ft_atoi(argv[2]);
+	data.time_to_eat = ft_atoi(argv[3]);
+	data.time_to_sleep = ft_atoi(argv[4]);
+	philos = malloc(data.num_philos * sizeof(t_philo));
+	if (!philos)
+		return (printf("Error: Memory allocation failed\n"), 1);
+	data.forks = malloc(data.num_philos * sizeof(pthread_mutex_t));
+	if (!data.forks)
+		return (printf("Error: Memory allocation failed\n"), free(philos), 1);
+	i = 0;
+	while (i++ < data.num_philos)
 	{
-		printf("Error: Invalid number of arguments\n");
-		return (1);
+		pthread_mutex_init(&data.forks[i], NULL);
+		philos[i].id = i;
+		philos[i].left_fork = &data.forks[i];
+		philos[i].right_fork = &data.forks[(i + 1) % data.num_philos];
+		i++;
 	}
-	else
-	{
-		data.num_philos = ft_atoi(argv[1]);
-		data.time_to_die = ft_atoi(argv[2]);
-		data.time_to_eat = ft_atoi(argv[3]);
-		data.time_to_sleep = ft_atoi(argv[4]);
-		while (data.num_philos > i)
-		{
-			philos[i].id = i;
-			philos[i].left_fork = &data.forks[i];
-			philos[i].right_fork = &data.forks[(i + 1) % data.num_philos];
-		}
-	}
+	i = 0;
+	while (i++ < data.num_philos)
+		pthread_mutex_destroy(&data.forks[i]);
+	free(data.forks);
+	free(philos);
 	return (0);
 }
